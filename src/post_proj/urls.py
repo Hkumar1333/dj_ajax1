55d4cdc7bd3ddfb095001491ee79f94228d1bd2f
Harmeek Kumar <Hkumar1333@conestogac.on.ca>
2024-04-06 19:13:11 -0400

    Modifided and created files as tutorial

--- src/post_proj/urls.py
+++ src/post_proj/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for post_proj project.
+URL configuration for posts_proj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,13 +15,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include 
-from django.conf import settings
+from django.urls import path,include
 from django.conf.urls.static import static
-
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(('posts.urls', 'posts'), namespace='posts'))
+
 ]
 
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+= static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+urlpatterns+= static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
