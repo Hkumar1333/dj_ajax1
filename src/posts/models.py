b3a4682ee749cd3bfcb00f875a75f7bea381c78f
Harmeek Kumar <Hkumar1333@conestogac.on.ca>
2024-04-06 18:02:32 -0400

    copied update and create then added strings in posts

--- src/posts/models.py
+++ src/posts/models.py
@@ -7,4 +7,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200) 
     body= models.TextField()
     liked = models.ManyToManyField (User, blank=True) 
-   
+    author = models.ForeignKey(Profile,on_delete=models.CASCADE)
+    updated = models.DateTimeField(auto_now=True) 
+    created = models.DateTimeField(auto_now_add=True)
+    
+    def ___str__(self):
+        return str(self.title)
+    
+    @property
+    def like_count(self):
+        return self.liked.all().count()
